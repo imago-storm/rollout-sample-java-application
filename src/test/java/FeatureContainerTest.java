4cd16ff32378c7dbd226e3484f5f90353d509b86
Polina Shubina <925043@gmail.com>
2020-04-09 18:54:54 +0300

    Testing probability

--- src/test/java/FeatureContainerTest.java
+++ src/test/java/FeatureContainerTest.java
@@ -1,19 +1,26 @@
 import com.cloudbees.rollout.FeatureContainer;
 import io.rollout.rox.server.Rox;
 import org.junit.Before;
+import org.junit.FixMethodOrder;
 import org.junit.Ignore;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class FeatureContainerTest {
 
     FeatureContainer flags;
+    private static boolean once = false;
 
     @Before
     public void setup() {
+        if (once) {
+            return;
+        }
         flags = new FeatureContainer();
         String envKey = System.getProperty("envKey", "5e85f93628de8e40325ae49b");
-        Rox.setup(envKey);
         Rox.register("", flags);
+        Rox.setup(envKey);
         try {
             Thread.sleep(1000 * 5);
         } catch (InterruptedException e) {
@@ -21,6 +28,7 @@ public class FeatureContainerTest {
         }
         //It needs time
         System.out.println(flags.mySampleFlag.isEnabled());
+        once = true;
     }
 
     @Test
@@ -28,4 +36,20 @@ public class FeatureContainerTest {
         assert flags.mySampleFlag.isEnabled();
     }
 
+    @Test
+    public void testProbabiltyFlag() {
+        String env = System.getProperty("env", "QA");
+        if (!env.equals("Production")) {
+            return;
+        }
+        int trues = 0;
+        for(int i = 0; i < 100; i++) {
+            boolean value = flags.mySampleFlag.isEnabled();
+            if (value) {
+                trues++;
+            }
+        }
+        System.out.println("Got true " + trues + " times");
+        assert Math.abs(trues - 10) < 5;
+    }
 }
